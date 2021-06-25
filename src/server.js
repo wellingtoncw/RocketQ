852eb5ab4c14bf234216ce7034d2b198996da93a
wellingtoncw <wellingtoncw7@gmail.com>
2021-06-25 19:39:49 -0300

    instala dependencias e cria novas rotas

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const server = express() //inicializa o express e guarda na const server
 
 server.set('view engine', 'ejs') // define pro express que a view engine é a ejs
 
+server.use(express.static("public")) //usa a pasta onde ficam guardados os arquivos staticos
+
 //define para o express que o caminho não é mais somente e pasta views mas o caminho roquetq/src/views
 server.set('views', path.join(__dirname, 'views')) //path pega o caminho da pasta onde está o projeto. O join é de juntar. __dirname é uma variável global que se transforma na pasta onde o arquivo atual está. Ou seja, (__dirname = src/). No final junta a pasta view. Caminho final = rocketq/src/views
 
