68faf62d56941f611aa4e10e128f18f80c70bce4
wellingtoncw <wellingtoncw7@gmail.com>
2021-06-26 12:13:29 -0300

    cria Room Controller

--- src/route.js
+++ src/route.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router() // a const route guarda todas as funcionalidades de Router que o express possui
 
-route.get('/', (req, res) => res.render('index')) // a barra é pra pegar o arquivo inicial do projeto (index). Definição de rotas
+route.get('/', (req, res) => res.render('index', {page: 'enter-room'})) // a barra é pra pegar o arquivo inicial do projeto (index). Definição de rotas
+route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
+
 route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
 //Formato que o formulário de dentro da modal tem que passar a informação
-route.post('/room/:room/:question/:action', QuestionController.index) // os : é uma variável pra receber o conteúdo que virá da outra pagina 
+route.post('/room/:room/:question/:action', QuestionController.index) // os : é uma variável pra receber o conteúdo que virá da outra pagina
+rout.post('/room/create-room', RoomController.create)
 module.exports = route //exporta a constante route
