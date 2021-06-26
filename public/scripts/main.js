2ff4f0991efdf024234cd1374e9ef9ecec25eae2
wellingtoncw <wellingtoncw7@gmail.com>
2021-06-26 10:53:30 -0300

    finaliza funções da modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,13 +2,31 @@ import  Modal  from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector(".modal h2")
+const modalDescription = document.querySelector(".modal p")
+const modalButton = document.querySelector(".modal button")
+
 //Pegar todos os botões que existem com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check")
 //Pegar quando o marcar como lido for clicado
 checkButtons.forEach(button => {
     //adicionar escutar
-    button.addEventListener("click", event => {
-        //Abrir modal
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
+
+//Quando o botão delete for clicado, ele abre a modal
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true) {
+    event.preventDefault() // para a tag a não se comportar como link
+    const text = check ? "Marcar como lida" : "Excluir"
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = `Ter certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    modal.open()
+}
