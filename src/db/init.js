902798fdedc65d20352445fb3584eecd458ba117
wellingtoncw <wellingtoncw7@gmail.com>
2021-06-28 19:38:25 -0300

    cria tabelas no banco

--- src/db/init.js
+++ src/db/init.js
@@ -1,9 +1,22 @@
 const Database = require('./config')
 
 const initDb = {
-    async init(){
-        const db = await Database()
+    async init(){ //pra usar o await em uma função, a função tem que ser async
+        const db = await Database() //await aguarda o retorno antes de ir pra proxima linha
 
-        await db.exec()
+        await db.exec(`CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+            )`);
+
+        await db.exec(`CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT,
+            read INT
+        )`);
+
+        await db.close()
     }
 }
+
+initDb.init()
