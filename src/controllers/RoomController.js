5c2a3c719417ca351740420abbe7a13fc437fd83
wellingtoncw <wellingtoncw7@gmail.com>
2021-06-29 21:35:23 -0300

    finaliza projeto

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,25 +5,56 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
 
-        for(var i = 0; i < 6; i++) {
-            //toString pra concatenar os 6 numeros
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-            roomId += Math.floor(Math.random() * 10).toString()
-            
+        while(isRoom) {
+
+            // Gera o número da sala
+            for(var i = 0; i < 6; i++) {
+                //toString pra concatenar os 6 numeros
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+                roomId += Math.floor(Math.random() * 10).toString()
+                
+            }
+            // Verificar se o número gerado já existe
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+    
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+            if(! isRoom){
+                // Inseri no banco
+                db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
         }
 
-        console.log
-
-        db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
-
         db.close()
+
+        res.redirect(`/room/${roomId}`)
+    },
+
+    async open(req, res){
+        const db = await Database()
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        let isNoQuestions
+
+        if(questions.length == 0){
+            if(questionsRead.length == 0){
+                isNoQuestions = true
+            }
+        }
+        res.render('room', {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
+    },
+
+    enter(req, res){
+        const roomId = req.body.roomId
 
         res.redirect(`/room/${roomId}`)
     }
--- src/route.js
+++ src/route.js
@@ -7,8 +7,11 @@ const route = express.Router() // a const route guarda todas as funcionalidades 
 route.get('/', (req, res) => res.render('index', {page: 'enter-room'})) // a barra é pra pegar o arquivo inicial do projeto (index). Definição de rotas
 route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render('room'))
-//Formato que o formulário de dentro da modal tem que passar a informação
-route.post('/question/:room/:question/:action', QuestionController.index) // os : é uma variável pra receber o conteúdo que virá da outra pagina
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index) // os : é uma variável pra receber o conteúdo que virá da outra pagina
+
 module.exports = route //exporta a constante route
